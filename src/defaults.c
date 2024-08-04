8b8ae9a10529094788cdfbb3fe44de0ecb2cf78b
Balazs Lengyak <balazs@lengyak.hu>
2024-08-04 12:34:11 +0200

    Make default dsp path and base note a defined value in header

--- src/defaults.c
+++ src/defaults.c
@@ -1,7 +1,2 @@
-const char *default_dsp_device_path = "/dev/dsp\0";
-
-/* means: 1/4, 1/8 etc. */
-const int default_base_note = 4;
-
 const int default_beat_per_minute[2] = {60, 4}; /* 60 bpm is given for 1/4 notes */
 const int default_beat_per_tact[2] = {1, 4}; /* beat per tact */
--- src/defaults.h
+++ src/defaults.h
@@ -1,13 +1,11 @@
 #ifndef CTRONOME_DEFAULTS_H
 #define CTRONOME_DEFAULTS_H
 
-extern const char *default_dsp_device_path;
-
-/* means: 1/4, 1/8 etc. */
-extern int default_base_note;
 // ASSETS_INSTALL_DIR is set by CMake, see CMakePresets.json and README.md for more information
 #define WAV1_DEFAULT_FILE_PATH ASSETS_INSTALL_DIR "/metronome1.wav"
 #define WAV2_DEFAULT_FILE_PATH ASSETS_INSTALL_DIR "/metronome2.wav"
+#define DSP_DEFAULT_DEVICE_PATH "/dev/dsp"
+#define DEFAULT_BASE_NOTE 4
 
 extern const int default_beat_per_minute[2];
 extern const int default_beat_per_tact[2];
